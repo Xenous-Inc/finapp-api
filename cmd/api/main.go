1fa35093cb5123a0718a4d89aa9d74f448480508
dr3dnought <sheremetyev48@mail.ru>
2023-12-10 19:43:20 +0300

    [Feat] Add cmd

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"github.com/Xenous-Inc/finapp-api/internal/server"
+	"github.com/Xenous-Inc/finapp-api/internal/di"
+	"github.com/Xenous-Inc/finapp-api/internal/utils/config"
+	"github.com/Xenous-Inc/finapp-api/internal/utils/flags"
 )
 
 func main() {
+	flags := flags.MustParseFlags()
+	config := config.MustLoadConfig(flags.EnvMode, flags.ConfigPath)
+	container := di.New(config)
+	server := container.GetServer()
 
-	server := server.NewServer()
-
-	err := server.ListenAndServe()
-	if err != nil {
-		panic("cannot start server")
-	}
+	server.StartListening()
 }
